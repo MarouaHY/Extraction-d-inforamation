7f8a5d39ec51e6a29540f2b6fc29fc08e40e7c9f
MarouaHY <167462582+MarouaHY@users.noreply.github.com>
2024-04-28 21:29:48 +0100

    Update iris-app.py

--- iris-app.py
+++ iris-app.py
@@ -4,21 +4,36 @@ import numpy as np
 import zipfile
 import os
 
-# Fonction pour calculer la distance euclidienne entre deux vecteurs
+# Function for calculating the Euclidean distance between two vectors
 def euclidean_distance(descriptor1, descriptor2):
     return np.linalg.norm(descriptor1 - descriptor2)
 
 # Titre de l'application
-st.title("Identification d'individus par l'iris")
+st.write('<h1 style="font-family:Arial; color:black; text-align:center;">Iris Identification System</h1>', unsafe_allow_html=True)
+st.write('<h5 style="font-family:Arial; color:grey; text-align:center;">Identify individuals based on their iris patterns.</h5>', unsafe_allow_html=True)
 
 # Chargement de l'image requête
-st.subheader("Chargement de l'image requête")
-image_requete_path = st.file_uploader("Veuillez sélectionner l'image requête", type=["jpg", "jpeg", "png"])
+st.subheader("**Upload an Iris Image**"  )
+image_requete_path = st.file_uploader("  ", type=["jpg", "jpeg", "png"])
 
-image = cv2.imdecode(np.frombuffer(image_requete_path.read(), np.uint8), cv2.IMREAD_COLOR)
-# Display original image
-st.header("Original Image")
-st.image(image, channels="BGR")       
+if image_requete_path is not None:  # Check if an image is uploaded
+    image = cv2.imdecode(np.frombuffer(image_requete_path.read(), np.uint8), cv2.IMREAD_COLOR)
+    
+    # Display original image
+    st.header("Original Image")
+    st.image(image, channels="BGR")
+    
+    # Processing details
+    if image is not None:  # Check if image is processed
+        st.write("Image processing details...")  # Display processing info
+    else:
+        st.info("En attente du traitement de l'image.")  # Informative message while processing
+    
+    # Rest of your code for processing the uploaded image...
+
+else:
+    st.info(" Please upload an image for identification.")  # Inform user to upload an image
+
 
 if image_requete_path:
     # Chargez l'image requête en niveaux de gris
@@ -28,33 +43,35 @@ if image_requete_path:
 
     image_requete = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    st.image(image_requete, caption="Image requête", use_column_width=True)
+    st.image(image_requete, caption="Grayscale Image of the Iris", use_column_width=True)
     
 
     # Extraction des caractéristiques SIFT de l'image requête
-    st.subheader("Extraction des caractéristiques SIFT de l'image requête")
+    st.subheader("SIFT Keypoint Detection")
     sift = cv2.SIFT_create()
     keypoints_requete, descripteurs_requete = sift.detectAndCompute(image_requete, None)
-    st.write(f"Nombre de keypoints dans l'image requête : {len(keypoints_requete)}")
-    st.write(f"Nombre de descripteurs dans l'image requête : {descripteurs_requete.shape[0]}")
+    st.write(f"Number of Keypoints Detected:  {len(keypoints_requete)}")
+    st.write(f"Number of Descriptors Extracted:  {descripteurs_requete.shape[0]}")
     
     
     st.subheader('Image with SIFT Keypoints')
 
     image_with_keypoints = cv2.drawKeypoints(image_requete, keypoints_requete, None)
-    st.image(image_with_keypoints, caption='Image with SIFT Keypoints', use_column_width=True)
+    st.image(image_with_keypoints, caption='Image Highlighting Keypoints', use_column_width=True)
     
     
     # Extraction des descripteurs de la base de données à partir du fichier ZIP
-    st.subheader("Extraction des descripteurs de la base de données à partir du fichier ZIP")
-    with zipfile.ZipFile("descripteurs_base_de_donnees.zip", "r") as zip_ref:
-        zip_ref.extractall("descripteurs_base_de_donnees_folder")
+    with st.spinner("Comparing Image Features..."):
+      st.subheader("Extracting Descriptors from Database")
+      with zipfile.ZipFile("descripteurs_base_de_donnees.zip", "r") as zip_ref:
+         zip_ref.extractall("descripteurs_base_de_donnees_folder")
+         
     
     # Charger les descripteurs extraits
     database_descriptors = np.load(os.path.join("descripteurs_base_de_donnees_folder", "descripteurs_base_de_donnees.npy"))
 
     # Seuil pour décider de l'acceptation ou du rejet
-    threshold = 8000  # À définir selon vos besoins
+    threshold = 8500  # À définir selon vos besoins
 
     # Calcul des distances euclidiennes entre le descripteur de l'image requête et chaque modèle
     distances = [euclidean_distance(descripteurs_requete, descriptor) for descriptor in database_descriptors]
@@ -66,8 +83,9 @@ if image_requete_path:
     # Vérifier si la distance minimale est inférieure au seuil donné
     if distances[min_distance_index] < threshold:
         # La correspondance est réussie, la personne est identifiée avec succès
-        st.success("La personne a été identifiée avec succès !")
-        st.write(f"Indice du modèle correspondant : {min_distance_index}")
+        st.success("Individual Identified Successfully!")
+        with st.container():
+                    st.write(f"**Matching Database Model Index**: {min_distance_index}")
     else:
         # La correspondance a échoué, la personne n'est pas identifiée
-        st.error("La personne n'a pas été identifiée.")
+        st.error("Individual Not Identified in Database.")
